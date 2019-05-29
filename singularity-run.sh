4c5ffeb37f3e698947088c9157c03605a92cd7db
Ashley Sommer <Ashley.Sommer@csiro.au>
2019-05-29 17:13:57 +1000

    more fix up to singularity-run.sh

--- singularity-run.sh
+++ singularity-run.sh
@@ -1,41 +1,73 @@
 #!/bin/bash
 #singularity-run.sh
-if [ $EUID != 0 ]; then
+#CONFIGURABLE VARS
+COSMOZ_REST_API_HOST_PORT=8083
+TIMESERIESDB_DIR="./tsdb"
+DOCUMENTDB_DIR="./mgdb"
+#END CONFIGURABLE VARS
+if [ ! "$EUID" -eq "0" ]; then
     echo "This singularity orchestration script MUST be run as superuser."
     echo "Elevating previleges."
     echo "If requested, please input your sudo password, or press ctrl+c to cancel."
     sudo "$0" "$@"
     exit $?
 fi
-if [ ! -f ./alpine39.sif ]; then
-    echo "Building alpine container image."
-    sudo singularity build alpine39.sif library://alpine:3.9
+REBUILD=false
+if [ "$1" = "stop" ]; then
+    sudo singularity instance stop cosmoz-influx-inst
+    sudo singularity instance stop cosmoz-mongo-inst
+    sudo singularity instance stop cosmoz-rest-inst
+    exit 0
+fi
+OLD_PWD="$PWD"
+cd ./singularity
+if [ "$1" = "rebuild" ]; then
+    sudo singularity build mongodb.sif mongodb.Singularity
+    sudo singularity build influxdb.sif influxdb.Singularity
+    sudo singularity build cosmoz-app.sif application.Singularity
 fi
 
+
+if [ ! -f mongodb.sif ]; then
+    echo "Building mongodb container image."
+    sudo singularity build mongodb.sif mongodb.Singularity
+fi
+if [ ! -f influxdb.sif ]; then
+    echo "Building influxdb container image."
+    sudo singularity build influxdb.sif influxdb.Singularity
+fi
+if [ ! -f cosmoz-app.sif ]; then
+    echo "Building cosmoz-rest-wrapper container image."
+    sudo singularity build cosmoz-app.sif application.Singularity
+fi
+cd "$OLD_PWD"
 echo "First trying to stop existing cosmoz mongodb container"
-sudo singularity instance stop t1
-sudo singularity instance start --writable-tmpfs --net --network bridge --hostname "t1" alpine39.sif t1
-T1_IP_ADDR="$(sudo singularity exec instance://t1 ip addr|awk -F'[ \n\t/]+' '/global/ { print $3 }')"
-echo $T1_IP_ADDR
-echo "First trying to stop existing cosmoz influxdb container"
-sudo singularity instance stop t2
-sudo singularity instance start --writable-tmpfs --net --network bridge --hostname "t2" alpine39.sif t2
-T2_IP_ADDR="$(sudo singularity exec instance://t2 ip addr|awk -F'[ \n\t/]+' '/global/ { print $3 }')"
-echo $T2_IP_ADDR
-echo "First trying to stop existing cosmoz influxdb container"
-sudo singularity instance stop t3
-sudo rm -f ./dummy_hosts.txt
-echo "127.0.0.1       localhost" > ./dummy_hosts.txt
-echo "127.0.1.1       local.cosmoz.csiro.au      local" >> ./dummy_hosts.txt
-echo "$T2_IP_ADDR   t2.cosmoz.csiro.au      t2" >> ./dummy_hosts.txt
-echo "$T1_IP_ADDR   t1.cosmoz.csiro.au      t1" >> ./dummy_hosts.txt
-sudo singularity instance start --writable-tmpfs --net --network bridge --hostname "t3" --bind ./dummy_hosts.txt:/etc/hosts alpine39.sif t3
-sudo singularity exec instance://t3 cat /etc/hosts
-sudo singularity exec instance://t3 ping t1
-#sudo singularity exec instance://t3 ping $T2_IP_ADDR
+sudo singularity instance stop cosmoz-mongo-inst
+SINGULARITYENV_MONGODB_RUN="/var/run/mongodb"
+SINGULARITYENV_MONGODB_DATA="/var/lib/mongodb"
+SINGULARITYENV_MONGODB_USER="mongodb"
+SINGULARITYENV_MONGODB_IP="0.0.0.0"
+SINGULARITYENV_MONGODB_OPTIONS="--journal"
+sudo singularity instance start --cleanenv --writable-tmpfs --bind "${DOCUMENTDB_DIR}:/data/db" --net --network bridge --hostname "cosmoz.mongodb" ./singularity/mongodb.sif cosmoz-mongo-inst
+T1_IP_ADDR="$(sudo singularity exec instance://cosmoz-mongo-inst ip addr|awk -F'[ \n\t/]+' '/global/ { print $3 }')"
+echo "Cosmoz mongodb IP is: $T1_IP_ADDR"
 
+echo "First trying to stop existing cosmoz influxdb container"
+sudo singularity instance stop cosmoz-influx-inst
+SINGULARITYENV_INFLUX_DB="cosmoz"
+sudo singularity instance start --cleanenv --writable-tmpfs --bind "${TIMESERIESDB_DIR}:/var/lib/influxdb" --bind "influxdb.conf:/etc/influxdb/influxdb.conf" --net --network bridge --hostname "cosmoz.influxdb" ./singularity/influxdb.sif cosmoz-influx-inst
+T2_IP_ADDR="$(sudo singularity exec instance://cosmoz-influx-inst ip addr|awk -F'[ \n\t/]+' '/global/ { print $3 }')"
+echo "Cosmoz influxdb IP is: $T2_IP_ADDR"
 
-#sudo singularity exec instance://t1 ping google.com
+echo "First trying to stop existing cosmoz rest wrapper container"
+sudo singularity instance stop cosmoz-rest-inst
+sudo rm -f ./singularity/dummy_hosts.txt
+echo "127.0.0.1       localhost" > ./singularity/dummy_hosts.txt
+echo "127.0.1.1       local.cosmoz.csiro.au      local" >> ./singularity/dummy_hosts.txt
+echo "${T2_IP_ADDR}   influx.cosmoz.csiro.au     cosmoz.influxdb" >> ./singularity/dummy_hosts.txt
+echo "${T1_IP_ADDR}   mongo.cosmoz.csiro.au      cosmoz.mongodb" >> ./singularity/dummy_hosts.txt
+sudo singularity instance start --cleanenv --writable-tmpfs --net --network bridge --hostname "cosmoz.rest" --network-args "portmap=${COSMOZ_REST_API_HOST_PORT}:8080/tcp" --bind ./singularity/dummy_hosts.txt:/etc/hosts ./singularity/cosmoz-app.sif cosmoz-rest-inst
+
 
 
 
