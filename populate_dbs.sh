54dfba180f655c1e064d3f29c813953746ad1c1e
Ashley Sommer <ashleysommer@gmail.com>
2019-06-07 10:49:55 +1000

    add populate script

---
+++ populate_dbs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# pre-populates the influxdb and mongodb from historical data
+
+source ./scripts/venv/bin/activate
+OLD_PWD="$PWD"
+cd ./scripts
+PYTHONUNBUFFERED=1
+export PYTHONUNBUFFERED
+# do stations
+#python3 ./csv-mongodb-converter.py
+
+# do all silo, intensities, and raw_vals (7300 is 20 years of data)
+#python3 ./mssql-influx-converter.py -d 7300
+
+# do processing levels (7300 is backprocess 20 years)
+python3 ./cosmoz-process-levels.py -d 7300
+
+
+cd "$OLD_PWD"
