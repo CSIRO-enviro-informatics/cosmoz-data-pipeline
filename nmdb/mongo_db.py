1c8184f75fc96f8502e24a4215afaf09c1aac70e
Ashley Sommer <ashleysommer@gmail.com>
2020-10-02 10:29:16 +1000

    getting site number from IMEI number is now dependent on a point in time

--- nmdb/mongo_db.py
+++ nmdb/mongo_db.py
@@ -2,7 +2,7 @@
 #
 from pymongo import MongoClient
 from ._mongo_db_config import config as mongodb_config
-from .utils import isostring_to_datetime, sql_to_isostring
+from datetime import datetime
 
 #mongo_client = MongoClient(mongodb_config['DB_HOST'], int(mongodb_config['DB_PORT']))  # 27017
 
@@ -17,12 +17,36 @@ def make_mongo_client(new=False):
 make_mongo_client.cached = None
 
 
-def get_site_no_from_imei(imei, mongo_client=None):
+def get_site_no_from_imei(imei, at_date=None, mongo_client=None):
     if mongo_client is None:
         mongo_client = make_mongo_client()
     mdb = getattr(mongo_client, mongodb_config['DB_NAME'])
     all_stations = mdb.all_stations
-    found_site = all_stations.find_one({'imei': imei})
+    found_sites = all_stations.find({'imei': imei})
+    found_sites = [f for f in found_sites]
+    if len(found_sites) < 1:
+        return 0
+    elif len(found_sites) < 2:
+        found_site = found_sites[0]
+    elif at_date is None:
+        # get the one most recently installed
+        found_sites.sort(key=lambda x: x.get('installation_date', None), reverse=True)
+        found_site = found_sites[0]
+    else:
+        # get the one most recently installed but before sent_date
+        found_sites.sort(key=lambda x: x.get('installation_date', None), reverse=True)
+        found_site = found_sites[-1]
+        if isinstance(at_date, datetime):
+            at_date = at_date.date()
+        for f in found_sites:
+            i_date = f.get('installation_date', None)
+            if i_date is None:
+                continue
+            if isinstance(i_date, datetime):
+                i_date = i_date.date()
+            if i_date <= at_date:
+                found_site = f
+                break
     if not found_site:
         return 0
     try:
